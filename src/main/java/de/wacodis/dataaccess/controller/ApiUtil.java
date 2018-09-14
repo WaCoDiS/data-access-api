f24c5bf4941317b22a681b051cbeac9c0d7626d8
arnevogt <arne.vogt@hs-bochum.de>
2018-09-14 16:27:19 +0200

    First Commit

---
+++ src/main/java/de/wacodis/dataaccess/controller/ApiUtil.java
@@ -0,0 +1,17 @@
+package de.wacodis.dataaccess.controller;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.context.request.NativeWebRequest;
+
+public class ApiUtil {
+    public static void setExampleResponse(
+            NativeWebRequest req, String contentType, String example) {
+        try {
+            req.getNativeResponse(HttpServletResponse.class).addHeader("Content-Type", contentType);
+            req.getNativeResponse(HttpServletResponse.class).getOutputStream().print(example);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/de/wacodis/dataaccess/model/AbstractResource.java
@@ -0,0 +1,140 @@
+package de.wacodis.dataaccess.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.Objects;
+import javax.validation.constraints.*;
+
+/** AbstractResource */
+@javax.annotation.Generated(
+        value = "org.openapitools.codegen.languages.SpringCodegen",
+        date = "2018-09-14T15:57:11.999+02:00[Europe/Berlin]")
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "method",
+        visible = true)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = PostResource.class, name = "PostResource"),
+    @JsonSubTypes.Type(value = GetResource.class, name = "GetResource"),
+})
+public class AbstractResource {
+    @JsonProperty("url")
+    private String url = null;
+
+    /** Gets or Sets method */
+    public enum MethodEnum {
+        GETRESOURCE("GetResource"),
+
+        POSTRESOURCE("PostResource");
+
+        private String value;
+
+        MethodEnum(String value) {
+            this.value = value;
+        }
+
+        @Override
+        @JsonValue
+        public String toString() {
+            return String.valueOf(value);
+        }
+
+        @JsonCreator
+        public static MethodEnum fromValue(String text) {
+            for (MethodEnum b : MethodEnum.values()) {
+                if (String.valueOf(b.value).equals(text)) {
+                    return b;
+                }
+            }
+            throw new IllegalArgumentException("Unexpected value '" + text + "'");
+        }
+    }
+
+    @JsonProperty("method")
+    private MethodEnum method = null;
+
+    public AbstractResource url(String url) {
+        this.url = url;
+        return this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return url
+     */
+    @ApiModelProperty(required = true, value = "")
+    @NotNull
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public AbstractResource method(MethodEnum method) {
+        this.method = method;
+        return this;
+    }
+
+    /**
+     * Get method
+     *
+     * @return method
+     */
+    @ApiModelProperty(required = true, value = "")
+    @NotNull
+    public MethodEnum getMethod() {
+        return method;
+    }
+
+    public void setMethod(MethodEnum method) {
+        this.method = method;
+    }
+
+    @Override
+    public boolean equals(java.lang.Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AbstractResource abstractResource = (AbstractResource) o;
+        return Objects.equals(this.url, abstractResource.url)
+                && Objects.equals(this.method, abstractResource.method);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url, method);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class AbstractResource {\n");
+
+        sb.append("    url: ").append(toIndentedString(url)).append("\n");
+        sb.append("    method: ").append(toIndentedString(method)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces (except the first
+     * line).
+     */
+    private String toIndentedString(java.lang.Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+}
