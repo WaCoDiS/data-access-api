4c31464b658e27dd596e00268cee6c35efb7d918
Arne Vogt <arne.vogt@hs-bochum.de>
2019-11-26 13:06:10 +0100

    add model classes to repo

---
+++ data-access-models/src/main/java/de/wacodis/dataaccess/model/ProcessingMetadata.java
@@ -0,0 +1,169 @@
+package de.wacodis.dataaccess.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import de.wacodis.dataaccess.model.AbstractDataEnvelopeAreaOfInterest;
+import de.wacodis.dataaccess.model.AbstractDataEnvelopeTimeFrame;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.joda.time.DateTime;
+import java.io.Serializable;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * extensible datatype for metadata that describes the processing of a product 
+ */
+@ApiModel(description = "extensible datatype for metadata that describes the processing of a product ")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-11-26T12:42:51.508+01:00[Europe/Berlin]")
+
+public class ProcessingMetadata  implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @JsonProperty("areaOfInterest")
+  private AbstractDataEnvelopeAreaOfInterest areaOfInterest = null;
+
+  @JsonProperty("timeFrame")
+  private AbstractDataEnvelopeTimeFrame timeFrame = null;
+
+  @JsonProperty("created")
+  private DateTime created = null;
+
+  @JsonProperty("modified")
+  private DateTime modified = null;
+
+  public ProcessingMetadata areaOfInterest(AbstractDataEnvelopeAreaOfInterest areaOfInterest) {
+    this.areaOfInterest = areaOfInterest;
+    return this;
+  }
+
+  /**
+   * Get areaOfInterest
+   * @return areaOfInterest
+  **/
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+  @Valid
+
+  public AbstractDataEnvelopeAreaOfInterest getAreaOfInterest() {
+    return areaOfInterest;
+  }
+
+  public void setAreaOfInterest(AbstractDataEnvelopeAreaOfInterest areaOfInterest) {
+    this.areaOfInterest = areaOfInterest;
+  }
+
+  public ProcessingMetadata timeFrame(AbstractDataEnvelopeTimeFrame timeFrame) {
+    this.timeFrame = timeFrame;
+    return this;
+  }
+
+  /**
+   * Get timeFrame
+   * @return timeFrame
+  **/
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+  @Valid
+
+  public AbstractDataEnvelopeTimeFrame getTimeFrame() {
+    return timeFrame;
+  }
+
+  public void setTimeFrame(AbstractDataEnvelopeTimeFrame timeFrame) {
+    this.timeFrame = timeFrame;
+  }
+
+  public ProcessingMetadata created(DateTime created) {
+    this.created = created;
+    return this;
+  }
+
+  /**
+   * time on which the dataset was created 
+   * @return created
+  **/
+  @ApiModelProperty(required = true, value = "time on which the dataset was created ")
+  @NotNull
+
+  @Valid
+
+  public DateTime getCreated() {
+    return created;
+  }
+
+  public void setCreated(DateTime created) {
+    this.created = created;
+  }
+
+  public ProcessingMetadata modified(DateTime modified) {
+    this.modified = modified;
+    return this;
+  }
+
+  /**
+   * time on which the dataset was modified last 
+   * @return modified
+  **/
+  @ApiModelProperty(required = true, value = "time on which the dataset was modified last ")
+  @NotNull
+
+  @Valid
+
+  public DateTime getModified() {
+    return modified;
+  }
+
+  public void setModified(DateTime modified) {
+    this.modified = modified;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ProcessingMetadata processingMetadata = (ProcessingMetadata) o;
+    return Objects.equals(this.areaOfInterest, processingMetadata.areaOfInterest) &&
+        Objects.equals(this.timeFrame, processingMetadata.timeFrame) &&
+        Objects.equals(this.created, processingMetadata.created) &&
+        Objects.equals(this.modified, processingMetadata.modified);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(areaOfInterest, timeFrame, created, modified);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ProcessingMetadata {\n");
+    
+    sb.append("    areaOfInterest: ").append(toIndentedString(areaOfInterest)).append("\n");
+    sb.append("    timeFrame: ").append(toIndentedString(timeFrame)).append("\n");
+    sb.append("    created: ").append(toIndentedString(created)).append("\n");
+    sb.append("    modified: ").append(toIndentedString(modified)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
