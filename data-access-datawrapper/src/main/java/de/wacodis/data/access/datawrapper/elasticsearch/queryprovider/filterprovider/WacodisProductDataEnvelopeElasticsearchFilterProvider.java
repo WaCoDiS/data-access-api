6ddd1893664bbbf530e689fd6fbcaf61de94c74a
Arne Vogt <arne.vogt@hs-bochum.de>
2019-11-29 14:00:55 +0100

    hotfix WacodisProductDataEnvelopeElasticsearchFilterProvider

--- data-access-datawrapper/src/main/java/de/wacodis/data/access/datawrapper/elasticsearch/queryprovider/filterprovider/WacodisProductDataEnvelopeElasticsearchFilterProvider.java
+++ data-access-datawrapper/src/main/java/de/wacodis/data/access/datawrapper/elasticsearch/queryprovider/filterprovider/WacodisProductDataEnvelopeElasticsearchFilterProvider.java
@@ -8,6 +8,7 @@ package de.wacodis.data.access.datawrapper.elasticsearch.queryprovider.filterpro
 import de.wacodis.dataaccess.model.AbstractDataEnvelope;
 import de.wacodis.dataaccess.model.WacodisProductDataEnvelope;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
@@ -17,27 +18,27 @@ import org.elasticsearch.index.query.QueryBuilders;
  * @author Matthes Rieke
  */
 public class WacodisProductDataEnvelopeElasticsearchFilterProvider implements DataEnvelopeElasticsearchFilterProvider {
-    
-    private final static String PRODUCTCOLLECTION_ATTRIBUTE = "productCollection";
-    private final static String SERVICENAME_ATTRIBUTE = "serviceName";
+
     private final static String PRODUCTTYPE_ATTRIBUTE = "productType";
+    private final static String PROCESS_ATTRIBUTE = "process";
+    private final static String BACKENDTYPE_ATTRIBUTE = "backendType";
 
     @Override
     public List<QueryBuilder> buildFiltersForDataEnvelope(AbstractDataEnvelope envelope) {
         if (envelope instanceof WacodisProductDataEnvelope) {
-            List<QueryBuilder> queries = new ArrayList<>();
             WacodisProductDataEnvelope wacEnv = (WacodisProductDataEnvelope) envelope;
+            List<QueryBuilder> queries = new ArrayList<>();
 
-            QueryBuilder productCollectionQuery = QueryBuilders.termQuery(PRODUCTCOLLECTION_ATTRIBUTE, wacEnv.getProductCollection());
-            queries.add(productCollectionQuery);
-            QueryBuilder serviceNameQuery = QueryBuilders.termQuery(SERVICENAME_ATTRIBUTE, wacEnv.getServiceName());
-            queries.add(serviceNameQuery);
-            
-            if(wacEnv.getProductType() != null && !wacEnv.getProductType().trim().isEmpty()){
+            //QueryBuilder productCollectionQuery = QueryBuilders.termQuery(PRODUCTCOLLECTION_ATTRIBUTE, wacEnv.getProductCollection());
+            QueryBuilder processQuery = QueryBuilders.termQuery(PROCESS_ATTRIBUTE, wacEnv.getProcess());
+            queries.add(processQuery);
+            QueryBuilder backendTypeQuery = QueryBuilders.termQuery(BACKENDTYPE_ATTRIBUTE, wacEnv.getBackendType().getBackendType().name());
+
+            if (wacEnv.getProductType() != null && !wacEnv.getProductType().trim().isEmpty()) {
                 QueryBuilder productTypeQuery = QueryBuilders.termQuery(PRODUCTTYPE_ATTRIBUTE, wacEnv.getProductType());
                 queries.add(productTypeQuery);
             }
-            
+
             return queries;
         } else {
             throw new IllegalArgumentException("wrong type of DataEnvelope, envelope is of type " + envelope.getClass().getSimpleName() + ", expected " + WacodisProductDataEnvelope.class.getSimpleName());
