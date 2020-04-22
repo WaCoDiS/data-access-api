176cfee66fcd01cc7054d4f84a2eba00ddb408c9
Sebastian Drost <s.drost@52north.org>
2020-04-22 12:32:14 +0200

    Add CORS mappings

--- data-access-api/src/main/java/org/openapitools/OpenAPI2SpringBoot.java
+++ data-access-api/src/main/java/org/openapitools/OpenAPI2SpringBoot.java
@@ -1,25 +1,21 @@
 package org.openapitools;
 
-import de.wacodis.dataaccess.configuration.ElasticsearchResourcesAPIConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @RefreshScope
 @ComponentScan(basePackages = {"org.openapitools", "de.wacodis.dataaccess.controller" , "de.wacodis.dataaccess.configuration", "de.wacodis.dataaccess.messaging" , "de.wacodis.dataaccess.elasticsearch", "org.openapitools.configuration"})
 public class OpenAPI2SpringBoot implements CommandLineRunner {
-    
-    
+
+
     @Override
     public void run(String... arg0) throws Exception {
         if (arg0.length > 0 && arg0[0].equals("exitcode")) {
@@ -43,14 +39,14 @@ public class OpenAPI2SpringBoot implements CommandLineRunner {
 
     @Bean
     public WebMvcConfigurer webConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            /*@Override
+        return new WebMvcConfigurer() {
+            @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("Content-Type");
-            }*/
+            }
         };
     }
 
