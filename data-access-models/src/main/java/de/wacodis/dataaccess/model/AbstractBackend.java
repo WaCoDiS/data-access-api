2bce8629665a07bf905ef99332a1d9c53d5cd461
Arne Vogt <arne.vogt@hs-bochum.de>
2020-05-07 16:19:30 +0200

    fix annotations in models

--- data-access-models/src/main/java/de/wacodis/dataaccess/model/AbstractBackend.java
+++ data-access-models/src/main/java/de/wacodis/dataaccess/model/AbstractBackend.java
@@ -18,7 +18,7 @@ import javax.validation.constraints.*;
 @ApiModel(description = "abstract type for a backend that provides WaCoDiS products ")
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-03-13T10:15:17.701+01:00[Europe/Berlin]")
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "backendType", visible = true)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "backendType", visible = true)
 @JsonSubTypes({
   @JsonSubTypes.Type(value = ArcGISImageServerBackend.class, name = "ArcGISImageServerBackend"),
   @JsonSubTypes.Type(value = GeoServerBackend.class, name = "GeoServerBackend"),
