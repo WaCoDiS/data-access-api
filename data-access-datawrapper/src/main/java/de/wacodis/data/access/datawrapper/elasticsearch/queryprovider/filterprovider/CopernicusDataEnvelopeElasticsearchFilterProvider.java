00f050172bfc5ccf4dce4ec1018dced6b496fbe5
Arne Vogt <arne.vogt@hs-bochum.de>
2020-05-14 15:12:49 +0200

    match optional parameters of CopernicusSubsetDefinition/DataEnvelope

--- data-access-datawrapper/src/main/java/de/wacodis/data/access/datawrapper/elasticsearch/queryprovider/filterprovider/CopernicusDataEnvelopeElasticsearchFilterProvider.java
+++ data-access-datawrapper/src/main/java/de/wacodis/data/access/datawrapper/elasticsearch/queryprovider/filterprovider/CopernicusDataEnvelopeElasticsearchFilterProvider.java
@@ -7,6 +7,7 @@ package de.wacodis.data.access.datawrapper.elasticsearch.queryprovider.filterpro
 
 import de.wacodis.dataaccess.model.AbstractDataEnvelope;
 import de.wacodis.dataaccess.model.CopernicusDataEnvelope;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.elasticsearch.index.query.QueryBuilder;
@@ -17,21 +18,50 @@ import org.elasticsearch.index.query.QueryBuilders;
  * @author <a href="mailto:arne.vogt@hs-bochum.de">Arne Vogt</a>
  */
 public class CopernicusDataEnvelopeElasticsearchFilterProvider implements DataEnvelopeElasticsearchFilterProvider {
-    
+
+    //mandatory attributes
     private final static String DATASETID_ATTRIBUTE = "datasetId";
     private final static String SATELLITE_ATTRIBUTE = "satellite";
     private final static String PORTAL_ATTRIBUTE = "portal";
+    //optional attributes
+    private static final String INSTRUMENT_ATTRIBUTE = "instrument";
+    private static final String SENSORMODE_ATTRIBUTE = "sensorMode";
+    private static final String PRODUCTTYPE_ATTRIBUTE = "productType";
+    private static final String PRODUCTLEVEL_ATTRIBUTE = "productLevel";
 
     @Override
     public List<QueryBuilder> buildFiltersForDataEnvelope(AbstractDataEnvelope envelope) {
         if (envelope instanceof CopernicusDataEnvelope) {
+            List<QueryBuilder> filters;
             CopernicusDataEnvelope copernicusEnvelope = (CopernicusDataEnvelope) envelope;
 
+            //filters for mandatory attributes
             QueryBuilder datasetIdQuery = QueryBuilders.termQuery(DATASETID_ATTRIBUTE, copernicusEnvelope.getDatasetId());
             QueryBuilder satelliteQuery = QueryBuilders.termQuery(SATELLITE_ATTRIBUTE, copernicusEnvelope.getSatellite().toString());
             //QueryBuilder portalQuery = QueryBuilders.termQuery(PORTAL_ATTRIBUTE, copernicusEnvelope.getPortal().toString());
-            
-            return Arrays.asList(new QueryBuilder[]{datasetIdQuery, satelliteQuery});
+            filters = Arrays.asList(new QueryBuilder[]{datasetIdQuery, satelliteQuery});
+
+            //filters for optional attributes
+            List<QueryBuilder> optionalFilters = new ArrayList<>();
+            if (copernicusEnvelope.getInstrument() != null) {
+                QueryBuilder instrumentFilter = QueryBuilders.termQuery(INSTRUMENT_ATTRIBUTE, copernicusEnvelope.getInstrument());
+                optionalFilters.add(instrumentFilter);
+            }
+            if (copernicusEnvelope.getSensorMode() != null) {
+                QueryBuilder sensorModeFilter = QueryBuilders.termQuery(SENSORMODE_ATTRIBUTE, copernicusEnvelope.getSensorMode());
+                optionalFilters.add(sensorModeFilter);
+            }
+            if (copernicusEnvelope.getProductType() != null) {
+                QueryBuilder productTypeFilter = QueryBuilders.termQuery(PRODUCTTYPE_ATTRIBUTE, copernicusEnvelope.getProductType());
+                optionalFilters.add(productTypeFilter);
+            }
+            if (copernicusEnvelope.getProductLevel() != null) {
+                QueryBuilder productLevelFilter = QueryBuilders.termQuery(PRODUCTLEVEL_ATTRIBUTE, copernicusEnvelope.getProductLevel());
+                optionalFilters.add(productLevelFilter);
+            }
+
+            filters.addAll(optionalFilters);
+            return filters;
         } else {
             throw new IllegalArgumentException("wrong type of DataEnvelope, envelope is of type " + envelope.getClass().getSimpleName() + ", expected " + CopernicusDataEnvelope.class.getSimpleName());
         }
--- data-access-datawrapper/src/main/java/de/wacodis/data/access/datawrapper/elasticsearch/queryprovider/filterprovider/CopernicusSubsetDefinitionElasticsearchFilterProvider.java
+++ data-access-datawrapper/src/main/java/de/wacodis/data/access/datawrapper/elasticsearch/queryprovider/filterprovider/CopernicusSubsetDefinitionElasticsearchFilterProvider.java
@@ -17,24 +17,48 @@ import org.elasticsearch.index.query.QueryBuilders;
  * @author <a href="mailto:arne.vogt@hs-bochum.de">Arne Vogt</a>
  */
 public class CopernicusSubsetDefinitionElasticsearchFilterProvider implements SubsetDefinitionElasticsearchFilterProvider {
-    
+
+    //mandatory attributes
     private static final String SATELLITEFILTER_ATTRIBUTE = "satellite";
     private static final String CLOUDCOVERAGEFILTER_ATTRIBUTE = "cloudCoverage";
+    //optional attributes
+    private static final String INSTRUMENT_ATTRIBUTE = "instrument";
+    private static final String SENSORMODE_ATTRIBUTE = "sensorMode";
+    private static final String PRODUCTTYPE_ATTRIBUTE = "productType";
+    private static final String PRODUCTLEVEL_ATTRIBUTE = "productLevel";
 
     @Override
     public List<QueryBuilder> buildFiltersForSubsetDefinition(AbstractSubsetDefinition subset) {
         if (subset instanceof CopernicusSubsetDefinition) {
             CopernicusSubsetDefinition copernicusSubset = (CopernicusSubsetDefinition) subset;
             List<QueryBuilder> filters = new ArrayList<>();
-
+            
+            //filters for mandatory attributes
             QueryBuilder satelliteFilter = QueryBuilders.termQuery(SATELLITEFILTER_ATTRIBUTE, copernicusSubset.getSatellite().toString());
-            QueryBuilder cloudCoverageFilter = QueryBuilders.rangeQuery(CLOUDCOVERAGEFILTER_ATTRIBUTE).gte(0).lte(copernicusSubset.getMaximumCloudCoverage()); //cloud coverage must be between 0 and maximumCloudCoverage
-
+            //cloud coverage must be between 0 and maximumCloudCoverage
+            QueryBuilder cloudCoverageFilter = QueryBuilders.rangeQuery(CLOUDCOVERAGEFILTER_ATTRIBUTE).gte(0).lte(copernicusSubset.getMaximumCloudCoverage());
             filters.add(satelliteFilter);
             filters.add(cloudCoverageFilter);
 
-            return filters;
+            //filters for optional attributes
+            if (copernicusSubset.getInstrument() != null) {
+                QueryBuilder instrumentFilter = QueryBuilders.termQuery(INSTRUMENT_ATTRIBUTE, copernicusSubset.getInstrument());
+                filters.add(instrumentFilter);
+            }
+            if (copernicusSubset.getSensorMode() != null) {
+                QueryBuilder sensorModeFilter = QueryBuilders.termQuery(SENSORMODE_ATTRIBUTE, copernicusSubset.getSensorMode());
+                filters.add(sensorModeFilter);
+            }
+            if (copernicusSubset.getProductType() != null) {
+                QueryBuilder productTypeFilter = QueryBuilders.termQuery(PRODUCTTYPE_ATTRIBUTE, copernicusSubset.getProductType());
+                filters.add(productTypeFilter);
+            }
+            if (copernicusSubset.getProductLevel()!= null) {
+                QueryBuilder productLevelFilter = QueryBuilders.termQuery(PRODUCTLEVEL_ATTRIBUTE, copernicusSubset.getProductLevel());
+                filters.add(productLevelFilter);
+            }
 
+            return filters;
         } else {
             throw new IllegalArgumentException("wrong type of SubsetDefinition, subset is of type " + subset.getClass().getSimpleName() + ", expected " + CopernicusSubsetDefinition.class.getSimpleName());
         }
