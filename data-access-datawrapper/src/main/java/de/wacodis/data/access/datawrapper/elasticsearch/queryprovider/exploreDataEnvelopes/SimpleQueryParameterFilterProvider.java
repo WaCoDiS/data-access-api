d1071811f7b00a0ce40b87404bf65a5f30f30f54
Arne Vogt <arne.vogt@hs-bochum.de>
2020-05-28 13:37:50 +0200

    change handling of unknown comparator

--- data-access-datawrapper/src/main/java/de/wacodis/data/access/datawrapper/elasticsearch/queryprovider/exploreDataEnvelopes/SimpleQueryParameterFilterProvider.java
+++ data-access-datawrapper/src/main/java/de/wacodis/data/access/datawrapper/elasticsearch/queryprovider/exploreDataEnvelopes/SimpleQueryParameterFilterProvider.java
@@ -6,6 +6,7 @@
 package de.wacodis.data.access.datawrapper.elasticsearch.queryprovider.exploreDataEnvelopes;
 
 import de.wacodis.dataaccess.model.DataEnvelopeQueryQueryParams;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.RangeQueryBuilder;
 import org.elasticsearch.index.query.TermQueryBuilder;
@@ -13,18 +14,19 @@ import org.slf4j.LoggerFactory;
 
 /**
  * converts query parameter to elasticsearch filter
+ *
  * @author Arne
  */
-public class SimpleQueryParameterFilterProvider implements QueryParameterFilterProvider{
-    
+public class SimpleQueryParameterFilterProvider implements QueryParameterFilterProvider {
+
     private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(SimpleQueryParameterFilterProvider.class);
 
     @Override
     public QueryBuilder buildFilterForQueryParam(String fieldName, DataEnvelopeQueryQueryParams queryParam) {
         QueryBuilder filter;
-        
-        switch(queryParam.getComparator()){
-            
+
+        switch (queryParam.getComparator()) {
+
             case EQUALS:
                 filter = new TermQueryBuilder(fieldName, queryParam.getValue());
                 break;
@@ -41,12 +43,11 @@ public class SimpleQueryParameterFilterProvider implements QueryParameterFilterP
                 filter = new RangeQueryBuilder(fieldName).lte(queryParam.getValue());
                 break;
             default:
-                LOGGER.warn("unknown comparator {}, assume comparator {}, build TermQuery for field name {} to check for equality", queryParam.getComparator().toString(), DataEnvelopeQueryQueryParams.ComparatorEnum.EQUALS.toString(), fieldName);
-                filter = new TermQueryBuilder(fieldName, queryParam.getValue());
-                break;
+                LOGGER.error("handling of comparator {} is not implemented", queryParam.getComparator().toString());
+                throw new IllegalArgumentException("unknown comparator "+ queryParam.getComparator().toString() + ", cannot build query for query parameter " + fieldName);
         }
-        
+
         return filter;
     }
-    
+
 }
