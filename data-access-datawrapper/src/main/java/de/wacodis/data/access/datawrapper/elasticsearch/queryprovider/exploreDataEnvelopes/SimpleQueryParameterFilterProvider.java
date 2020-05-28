71e3cccd306e6a1f14a37c15667c0b4f6f1113af
Arne Vogt <arne.vogt@hs-bochum.de>
2020-05-28 17:10:12 +0200

    handle null values in queries

--- data-access-datawrapper/src/main/java/de/wacodis/data/access/datawrapper/elasticsearch/queryprovider/exploreDataEnvelopes/SimpleQueryParameterFilterProvider.java
+++ data-access-datawrapper/src/main/java/de/wacodis/data/access/datawrapper/elasticsearch/queryprovider/exploreDataEnvelopes/SimpleQueryParameterFilterProvider.java
@@ -6,7 +6,9 @@
 package de.wacodis.data.access.datawrapper.elasticsearch.queryprovider.exploreDataEnvelopes;
 
 import de.wacodis.dataaccess.model.DataEnvelopeQueryQueryParams;
+import static de.wacodis.dataaccess.model.DataEnvelopeQueryQueryParams.ComparatorEnum.*;
 import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.ExistsQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.RangeQueryBuilder;
 import org.elasticsearch.index.query.TermQueryBuilder;
@@ -24,30 +26,46 @@ public class SimpleQueryParameterFilterProvider implements QueryParameterFilterP
     @Override
     public QueryBuilder buildFilterForQueryParam(String fieldName, DataEnvelopeQueryQueryParams queryParam) {
         QueryBuilder filter;
+        Object value = queryParam.getValue();
+        DataEnvelopeQueryQueryParams.ComparatorEnum comparator = queryParam.getComparator();
 
-        switch (queryParam.getComparator()) {
+        //null value only allowed combined with comparator 'EQUALS' or 'NOT'
+        if (value == null && !(comparator.equals(EQUALS) || comparator.equals(NOT))) {
+            LOGGER.error("illegal null value for field '{}'", fieldName);
+            throw new IllegalArgumentException("null value of query param '" + fieldName + "' is not allowed combined with comparator '" + comparator.toString() + "', cannot build query for query parameter '" + fieldName + "'");
+        }
+
+        switch (comparator) {
 
             case EQUALS:
-                filter = new TermQueryBuilder(fieldName, queryParam.getValue());
+                if (value != null) {
+                    filter = new TermQueryBuilder(fieldName, queryParam.getValue());
+                } else { //field must not exist if source value is null
+                    filter = new BoolQueryBuilder().mustNot(new ExistsQueryBuilder(fieldName));
+                }
                 break;
             case GREATER:
-                filter = new RangeQueryBuilder(fieldName).gt(queryParam.getValue());
+                filter = new RangeQueryBuilder(fieldName).gt(value);
                 break;
             case LESSER:
-                filter = new RangeQueryBuilder(fieldName).lt(queryParam.getValue());
+                filter = new RangeQueryBuilder(fieldName).lt(value);
                 break;
             case GREATEROREQUALS:
-                filter = new RangeQueryBuilder(fieldName).gte(queryParam.getValue());
+                filter = new RangeQueryBuilder(fieldName).gte(value);
                 break;
             case LESSEROREQUALS:
-                filter = new RangeQueryBuilder(fieldName).lte(queryParam.getValue());
+                filter = new RangeQueryBuilder(fieldName).lte(value);
                 break;
             case NOT:
-                filter = new BoolQueryBuilder().mustNot(new TermQueryBuilder(fieldName, queryParam.getValue()));
+                if (value != null) {
+                    filter = new BoolQueryBuilder().mustNot(new TermQueryBuilder(fieldName, value));
+                } else { //field must exist if source value is not null
+                    filter = new ExistsQueryBuilder(fieldName);
+                }
                 break;
             default:
-                LOGGER.error("handling of comparator {} is not implemented", queryParam.getComparator().toString());
-                throw new IllegalArgumentException("unknown comparator " + queryParam.getComparator().toString() + ", cannot build query for query parameter " + fieldName);
+                LOGGER.error("handling of comparator '{}' is not implemented", queryParam.getComparator().toString());
+                throw new IllegalArgumentException("unknown comparator '" + queryParam.getComparator().toString() + "', cannot build query for query parameter '" + fieldName + "'");
         }
 
         return filter;
